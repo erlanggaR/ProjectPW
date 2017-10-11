2abcfb3c989fdae998a7f8317bdb9d73f61f3903
paquitaputri <paquitaputri0701@yahoo.com>
2017-10-11 12:21:06 +0700

    Nambahin Movie detail + css

---
+++ Nyoba.java
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Movie Detail</title>
+<link href="style2.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+
+<body>
+<div class="header">
+		<h1>Cynema 
+        <button class="tombol" type="button">Register</button> 
+        <button class="tombol" type="button">Login</button>
+
+      </h1>
+      
+    <ul>
+      <li><a href="#home">Cinema</a></li>
+      <li><a href="#news">Movie</a></li>
+      <div class="dropdown">
+  		<div class="dropdown-content">
+    	</div>
+	  </div>
+		
+    </ul> 
+	</div>
+<div id="styled_video_container">
+    <video src="Kingsman2.mp4" type="video/mp4" controls poster="/video/wind-8a1cd93f.png" id="styled_video" muted preload="metadata" loop>
+	<button type="button" id="style_it">Style It!</button>
+<button type="button" id="stop_style_it">Stop It!</button>
+</div>
+
+</body>
